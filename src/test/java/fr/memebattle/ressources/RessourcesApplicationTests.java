0495294cadf23a8eb66cab60e51a479b4915d256
Etienne Chevrollier <etienne.chevrollier@live.fr>
2023-05-25 09:25:40 -0400

    Init Project

---
+++ src/test/java/fr/memebattle/ressources/RessourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.memebattle.ressources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RessourcesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
